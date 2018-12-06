9ad52b693ab737cceabd25f2574b943d8cc6b9a9
Nathan Beck <nathan.beck@fleishman.com>
2018-12-06 14:07:34 -0600

    Build production-ready files

---
+++ LocalFile.js
@@ -0,0 +1,77 @@
+"use strict";
+
+const {
+  createRemoteFileNode
+} = require('gatsby-source-filesystem');
+
+class LocalFile {
+  constructor(createRemoteArgs) {
+    this.createRemoteArgs = createRemoteArgs;
+    this.fileFields = {
+      product: ["images"],
+      sku: ["image", "product.images"],
+      file: ["url"]
+    };
+  }
+
+  downloadImages(payload, type) {
+    const fields = this.fileFields[type.toLowerCase()];
+    fields.forEach(field => {
+      const splitPath = field.split(".");
+      let urls = this.getNestedObject(payload, splitPath);
+      if (!urls || !urls.length) return;
+      if (!Array.isArray(urls)) urls = [urls];
+      const sourceObject = splitPath.length >= 1 ? this.getNestedObject(payload, splitPath.slice(0, -1)) : payload;
+      sourceObject.localFiles___NODE = [];
+      urls.forEach(async url => {
+        let fileNode;
+
+        try {
+          fileNode = await createRemoteFileNode({
+            url,
+            ...this.createRemoteArgs
+          });
+        } catch (e) {
+          console.log(e);
+        }
+
+        if (fileNode) {
+          sourceObject.localFiles___NODE.push(fileNode.id);
+        }
+      });
+    });
+    return payload;
+  }
+
+  async downloadFiles(payload) {
+    const updatedData = payload.data.map(async file => {
+      let fileNode;
+
+      try {
+        fileNode = await createRemoteFileNode({
+          url: file.url,
+          ext: `.${file.type}`,
+          ...this.createRemoteArgs
+        });
+      } catch (e) {
+        console.log(e);
+      }
+
+      if (fileNode) {
+        file.localFiles___NODE = [fileNode.id];
+      }
+
+      return file;
+    });
+    payload.data = await Promise.all(updatedData);
+    return payload;
+  } // Access nested objects with path given as array.
+
+
+  getNestedObject(object, path) {
+    return path.reduce((obj, key) => obj[key], object);
+  }
+
+}
+
+module.exports = LocalFile;
---
+++ gatsby-node.js
@@ -0,0 +1,128 @@
+"use strict";
+
+const stripeClient = require('stripe');
+
+const StripeObject = require('./StripeObject');
+
+const LocalFile = require('./LocalFile');
+
+exports.sourceNodes = async ({
+  actions,
+  cache,
+  createNodeId,
+  createContentDigest,
+  store
+}, {
+  downloadFiles = false,
+  objects = [],
+  secretKey = ""
+}) => {
+  const {
+    createNode
+  } = actions;
+
+  if (!objects.length) {
+    console.error(new Error("No Stripe object types found in your gatsby-config. Add types to the objects array like this: ['Balance', 'Customer', 'BalanceTransaction']"));
+    return;
+  }
+
+  if (!secretKey) {
+    console.error(new Error("No Stripe secret key found in your gatsby-config."));
+    return;
+  }
+
+  const localFile = new LocalFile({
+    store,
+    cache,
+    createNode,
+    createNodeId,
+    // Currently createRemoteFileNode discards auth headers with empty passwords
+    auth: {
+      htaccess_user: secretKey,
+      htaccess_pass: "null"
+    }
+  });
+  const stripe = stripeClient(secretKey);
+  stripe.setAppInfo({
+    name: "Gatsby.js Stripe Source Plugin",
+    version: "2.0.0",
+    url: "https://www.npmjs.com/package/gatsby-source-stripe"
+  }); // Initialize stripeObjects based on gatsby plugin objects
+
+  const stripeObjects = objects.map(type => new StripeObject(type));
+
+  for (const stripeObj of stripeObjects) {
+    /*
+     * Outputs the stripe object's path, to allow us to
+     * get to the proper method
+     *
+     * Example outputs:
+     * const path = stripe['customers']
+     * const path = stripe['terminal']['readers']
+     */
+    const path = stripeObj.objectPath(stripe);
+    /*
+     * Some Stripe objects, for whatever reason, aren't iterable
+     * This check here, allows us to still get that data from Stripe
+     * The canIterate key is set manually in stripeObjects.json
+     * based on testing the different object types.
+     */
+
+    if (!stripeObj.canIterate) {
+      let payload = await path[stripeObj.methodName](stripeObj.methodArgs);
+
+      if (downloadFiles) {
+        payload = await localFile.downloadFiles(payload);
+      }
+
+      const node = stripeObj.node(createContentDigest, payload);
+      createNode(node);
+      continue;
+    }
+    /*
+     * Use for - await - of as per the Stripe.js Node documentation
+     * for auto pagination purposes
+     *
+     * path[stripeObj.methodName](stripeObj.methodArgs) translates to
+     * something like the following, depending on the object types
+     * passed in the config:
+     *
+     * stripe['customers']['list']({ "expand": "data.default_source" })
+     */
+
+
+    for await (let payload of path[stripeObj.methodName](stripeObj.methodArgs)) {
+      /**
+       * Leaving this in here as a reminder that, depending on what the Gatsby.js
+       * team says, I'll need to deal with event objects for skus and products having
+       * the same attributes key, but having different data types (i.e. sku.attributes
+       * is an object, and product.attributes is an array).
+       */
+
+      /*
+      if (payload.object == 'event' && Array.isArray(payload.data.object.attributes)) {
+        console.log(payload.data.object.object);
+        console.log(payload.data.object.attributes);
+      }
+      */
+
+      /*
+      * Download and create File nodes for object images, only if
+      * downloadFiles is configured.
+      *
+      * Adds the localFiles field, which is an array of
+      * references to the created File nodes.
+      *
+      * Currently only supports Product and Sku images.
+      */
+      if (downloadFiles) {
+        payload = localFile.downloadImages(payload, stripeObj.type);
+      }
+
+      const node = stripeObj.node(createContentDigest, payload);
+      createNode(node);
+    }
+  }
+
+  return;
+};
