5f065a22434e9b20feec90ec8d790dd9adff3bd3
Nathan Beck <njosefbeck@gmail.com>
2019-04-26 16:45:36 -0500

    Add logic to handle unknown type in StripeObject constructor

--- src/__tests__/stripeObject.test.js
+++ src/__tests__/stripeObject.test.js
@@ -1,12 +1,19 @@
 const StripeObject = require('../stripeObject')
 
 describe('StripeObject', () => {
-  test('initializes the right object given a known type', () => {
+  test('constructor initializes the right object given a known type', () => {
     const type = 'Sku'
     const instance = new StripeObject('Sku')
 
     expect(instance.name).toBe('skus')
     expect(instance.type).toBe('Sku')
     expect(instance.methodName).toBe('list')
+  })
+
+  test('constructor throws an error when given an unknown type', () => {
+    const type = 'Review'
+    const error = `Unknown type: ${type} passed to StripeObject. This type is not supported.`
+
+    expect(() => new StripeObject(type)).toThrow(error)
   })
 })
