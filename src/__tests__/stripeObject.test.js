2759e00adb8800b9ab3e12a72147c211631ffb5f
Nathan Beck <njosefbeck@gmail.com>
2019-04-26 16:47:02 -0500

    Reorganize test file

--- src/__tests__/stripeObject.test.js
+++ src/__tests__/stripeObject.test.js
@@ -1,19 +1,21 @@
 const StripeObject = require('../stripeObject')
 
 describe('StripeObject', () => {
-  test('constructor initializes the right object given a known type', () => {
-    const type = 'Sku'
-    const instance = new StripeObject('Sku')
+  describe('constructor', () => {
+    test('throws an error when given an unknown type', () => {
+      const type = 'Review'
+      const error = `Unknown type: ${type} passed to StripeObject. This type is not supported.`
 
-    expect(instance.name).toBe('skus')
-    expect(instance.type).toBe('Sku')
-    expect(instance.methodName).toBe('list')
-  })
+      expect(() => new StripeObject(type)).toThrow(error)
+    })
 
-  test('constructor throws an error when given an unknown type', () => {
-    const type = 'Review'
-    const error = `Unknown type: ${type} passed to StripeObject. This type is not supported.`
+    test('initializes the right object given a known type', () => {
+      const type = 'Sku'
+      const instance = new StripeObject('Sku')
 
-    expect(() => new StripeObject(type)).toThrow(error)
+      expect(instance.name).toBe('skus')
+      expect(instance.type).toBe('Sku')
+      expect(instance.methodName).toBe('list')
+    })
   })
 })
