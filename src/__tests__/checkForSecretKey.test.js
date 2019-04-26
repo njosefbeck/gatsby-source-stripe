34f9e93586cbbf401eb4b7542e961594c4b79cf2
Nathan Beck <njosefbeck@gmail.com>
2019-04-26 18:01:19 -0500

    Add test for checkForSecretKey.js

---
+++ src/__tests__/checkForSecretKey.test.js
@@ -0,0 +1,18 @@
+const checkForSecretKey = require('../checkForSecretKey')
+
+describe('plugin', () => {
+  describe(', when options: secretKey', () => {
+    test('is empty, throws error', () => {
+      const secretKey = ""
+      const error = "No Stripe secret key found in gatsby-config"
+
+      expect(() => checkForSecretKey(secretKey)).toThrow(error)
+    })
+
+    test('is not empty, does not throw error', () => {
+      const secretKey = "a_secret_key"
+
+      expect(checkForSecretKey(secretKey)).toBe(undefined)
+    })
+  })
+})
