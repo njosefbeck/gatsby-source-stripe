62040dcf4ed9207276d32441d1762cd7c26b5667
Nathan Beck <njosefbeck@gmail.com>
2019-05-05 18:11:56 -0500

    Make cacheKey test pass

--- src/__tests__/cacheKey.test.js
+++ src/__tests__/cacheKey.test.js
@@ -3,7 +3,7 @@ const cacheKey = require("../helpers/cacheKey");
 describe("cacheKey()", () => {
   test("returns Gatsby cache key when given file url", () => {
     const url = "https://files.stripe.com/files/f_test_SWXjBddZpg3sRYV6R9yVBpu3";
-    const expected = `create-remote-file-node-${url}`
+    const expected = `stripe-file-${url}`
 
     expect(cacheKey(url)).toBe(expected);
   })
