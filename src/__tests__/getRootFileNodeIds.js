aacab6ed366972bb5b3b9e343fc22b01a43a2efa
Nathan Beck <njosefbeck@gmail.com>
2019-05-05 18:56:13 -0500

    Add tests for getNon and getRoot FileNodeIds

---
+++ src/__tests__/getRootFileNodeIds.js
@@ -0,0 +1,28 @@
+const getRootFileNodeIds = require("../helpers/getRootFileNodeIds");
+
+describe("getRootFileNodeIds()", () => {
+  test("returns only root file node ids", () => {
+    const fileNodeIds = [
+      {
+        location: "root",
+        id: "8fa3f02d-5e34-5191-bf6a-7533210053fc",
+      },
+      {
+        location: "product",
+        id: "67735a50-5109-51be-85b4-803f05d27797"
+      },
+      {
+        location: "product",
+        id: "7bffbe3e-beed-5893-bc26-eaec9fc6d787"
+      },
+      {
+        location: "product",
+        id: "b9c3ae18-78f9-5b31-8e6b-372de7b6102a"
+      },
+    ];
+
+    const expected = ["8fa3f02d-5e34-5191-bf6a-7533210053fc"];
+
+    expect(getRootFileNodeIds(fileNodeIds)).toEqual(expected);
+  })
+})
