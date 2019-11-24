e953a74146856f40f0a648b4c30351432a79a433
Nathan Beck <njosefbeck@gmail.com>
2019-11-24 12:08:37 -0600

    Re-add ternary

--- src/helpers/hasFilesToDownload.js
+++ src/helpers/hasFilesToDownload.js
@@ -2,7 +2,8 @@ function hasFilesToDownload(node) {
   const hasImages = node.images && node.images.length;
   const hasImage = node.image && node.image.length;
   const hasProductImages = node.product && hasFilesToDownload(node.product);
-  return hasImages || hasImage || hasProductImages;
+  // eslint-disable-next-line no-unneeded-ternary
+  return hasImages || hasImage || hasProductImages ? true : false;
 }
 
 module.exports = hasFilesToDownload;
